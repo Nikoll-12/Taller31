909e161a096ae0f5badfacd8f329c57ad8882229
Nikoll-12 <est.nikoll.borja@unimilitar.edu.co>
2026-05-08 10:17:13 -0500

    Implementación del Canvas

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * =========================================================
+ * ALGORITMO DE COHEN - SUTHERLAND
+ * Recorte de líneas
+ * =========================================================
+ */
+
+/**
+ * Canvas y contexto gráfico
+ * ctx representa el espacio de dibujo 2D
+ */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
